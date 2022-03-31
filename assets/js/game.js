7981173596809f2de13624f628c24ed5941baec9
leanne rajan <leanne.raju261@gmail.com>
2022-03-31 14:59:33 -0700

    created function and variable in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+// to store user input using variables
+var playerName = window.prompt("What is your robot's name?");
+
+// Note the lack of quatation marks around playerName.
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+
+//this will do math and log 10
+console.log(10 + 10);
+
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+// to call the function- fight
+// fight ();
