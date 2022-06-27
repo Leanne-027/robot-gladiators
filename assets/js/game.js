5bf178995e5d16cf89d8715fb267926fba4ece43
leanne rajan <leanne.raju261@gmail.com>
2022-06-27 15:01:32 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,20 +4,18 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roberto";
+//Declare enemy-robot names
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // this creates a function named "fight"
-var fight = function() {
+var fight = function(enemyName) {
+    //Alert players that they are stating the round
     window.alert("Welcome to Robot Gladiators!");
 
 // ask player if they would like to fight or not
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-    console.log(promptFight);
 
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT"){
@@ -29,8 +27,7 @@ if (promptFight === "fight" || promptFight === "FIGHT"){
     // check enemy's health 
     if (enemyHealth <=0) {
         window.alert(enemyName + " has died!");
-    }
-    else {
+    } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
@@ -70,12 +67,14 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
     else {
         window.alert ("You need to choose a valid option. Try again");
     }
-};
 
+};
 // check to see if the value of the playerHealth variable is greater than 0
 if (playerHealth > 0) {
     console.log("Your player is still alive!")
 }
-
-// to call the function - fight
-fight ();
+    
+// Using for loops in array and calling the fight function
+    for(var i = 0; i < enemyNames.length; i++){
+        fight (enemyNames[i]);
+     }
